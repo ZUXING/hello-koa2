99fb0dcd10df4dacf0d039beaf6f484e101a7b34
ZUXING <zuxingv@foxmail.com>
2020-02-23 23:40:27 +0800

    了解什么是koa2和middleware

--- app.js
+++ app.js
@@ -7,12 +7,21 @@ const app = new Koa();
 // 对于任何请求，app将调用该异步函数处理请求：
 /*
 	什么是ctx：https://www.jianshu.com/p/a4fdfdf93472
-	简单来讲，ctx是
+	ctx是context的简称，你可以把ctx理解为浏览器内容的框。
 */
 app.use(async (ctx, next) => {
     await next();
     ctx.response.type = 'text/html';
     ctx.response.body = '<h1>Hello, koa2!</h1>';
+});
+
+/*
+	廖老师把一个async代码块称作一个middleware
+	这样设计的好处是结构化开发，虽然我还没感觉到怎么个结构化……
+*/
+app.use(async (ctx, next) => {
+    console.log(`${ctx.request.method} ${ctx.request.url}`); // 打印URL
+    await next(); // 调用下一个middleware
 });
 
 // 在端口3000监听:
