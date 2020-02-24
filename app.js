6e3a38d2c42c5d9ba76a3c3c4b28fe5d9863ad8f
ZUXING <zuxingv@foxmail.com>
2020-02-24 11:39:31 +0800

    一个支持表单、路由的koa2框架

--- app.js
+++ app.js
@@ -1,28 +1,66 @@
 // 导入koa，和koa 1.x不同，在koa2中，我们导入的是一个class，因此用大写的Koa表示:
 const Koa = require('koa');
 
+// 注意require('koa-router')返回的是函数:
+const router = require('koa-router')();	
+const bodyParser = require('koa-bodyparser');
+
 // 创建一个Koa对象表示web app本身:
 const app = new Koa();
+
+app.use(async (ctx, next) => {
+    console.log(`${ctx.request.method} ${ctx.request.url}`); // 打印URL
+    await next(); // 调用下一个middleware
+});
 
 // 对于任何请求，app将调用该异步函数处理请求：
 /*
 	什么是ctx：https://www.jianshu.com/p/a4fdfdf93472
 	ctx是context的简称，你可以把ctx理解为浏览器内容的框。
 */
-app.use(async (ctx, next) => {
-    await next();
-    ctx.response.type = 'text/html';
-    ctx.response.body = '<h1>Hello, koa2!</h1>';
+
+router.get('/hello/:name', async (ctx, next) => {
+    var name = ctx.params.name;
+    ctx.response.body = `<h1>Hello, ${name}!</h1>`;
 });
+
+router.get('/:name', async (ctx, next) => {
+    var name = ctx.params.name;
+    ctx.response.body = `<h1>Hello, ${name}!</h1>`;
+});
+
+router.get('/', async (ctx, next) => {
+    ctx.response.body = `<h1>Index</h1>
+        <form action="/signin" method="post">
+            <p>Name: <input name="name" value="koa"></p>
+            <p>Password: <input name="password" type="password"></p>
+            <p><input type="submit" value="Submit"></p>
+        </form>`;
+});
+
+router.post('/signin', async (ctx, next) => {
+    var
+        name = ctx.request.body.name || '',
+        password = ctx.request.body.password || '';
+    console.log(`signin with name: ${name}, password: ${password}`);
+    if (name === 'koa' && password === '12345') {
+        ctx.response.body = `<h1>Welcome, ${name}!</h1>`;
+    } else {
+        ctx.response.body = `<h1>Login failed!</h1>
+        <p><a href="/">Try again</a></p>`;
+    }
+});
+
+app.use(bodyParser());
+// add router middleware:
+app.use(router.routes());
+
 
 /*
 	廖老师把一个async代码块称作一个middleware
 	这样设计的好处是结构化开发，虽然我还没感觉到怎么个结构化……
 */
-app.use(async (ctx, next) => {
-    console.log(`${ctx.request.method} ${ctx.request.url}`); // 打印URL
-    await next(); // 调用下一个middleware
-});
+
 
 // 在端口3000监听:
 app.listen(3000);
